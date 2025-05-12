14371d3ed6be40d3cfde936a41fb68ff9bfdf161
Nero-977 <lukas.mayer@stud.akad.de>
2025-05-12 09:06:53 +0200

    TITEL GEÄNDERT

--- Projekt2/Startseite.php
+++ Projekt2/Startseite.php
@@ -18,7 +18,7 @@ $linkwechsel = 'Abmelden';
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>FEWO SEAHOUSES</title>
+    <title>FEWO SEAHOUSES TEST</title>
     <!-- Style -->
     <link rel="stylesheet" href="style.css" />
   </head>
